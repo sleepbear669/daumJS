df9e8f38f60abf2fbd493876401e96262642a440
sleepbear669 <sleepbear669@gmail.com>
2015-03-12 17:32:20 +0900

    add getId function and form listener

--- todos.js
+++ todos.js
@@ -1,3 +1,15 @@
 //start...
 
-console.log("hellow world");
+// console.log("hellow world");
+
+function getId(id){
+	return document.getElementById(id);
+};
+
+var todoStringField = getId("todoString");
+console.log(todoStringField, todoStringField.value);
+var form = document.getElementById('todoForm');
+
+form.addEventListener('keyup', function(){
+	console.log(todoStringField, todoStringField.value);
+});
