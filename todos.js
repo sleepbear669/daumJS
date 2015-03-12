ae27a85e14538f71c5b919ea15a61bf6da594b72
sleepbear669 <sleepbear669@gmail.com>
2015-03-12 18:04:23 +0900

    ul list add code

--- todos.js
+++ todos.js
@@ -11,5 +11,19 @@ console.log(todoStringField, todoStringField.value);
 var form = document.getElementById('todoForm');
 
 form.addEventListener('keyup', function(){
-	console.log(todoStringField, todoStringField.value);
+	if(event.keyCode === 13){
+		console.log(todoStringField.value);
+		var todoListValue = todoStringField.value;
+		todoStringField.value = null;
+		var todoListField = getId("todoList");
+		todoListField.innerHTML += '\
+           <li>\
+                <button class="delete">×</button>\
+                <input type="checkbox" class="toggle-checked">\
+                <span class="text">' + todoListValue+' </span>\
+            </li>';
+	}
+	// console.log(event.keyCode);
+	// console.log(todoStringField, todoStringField.value);
+
 });
